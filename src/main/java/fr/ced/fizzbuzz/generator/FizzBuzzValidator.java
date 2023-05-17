f49d8ad95c19edb04a64f9acfebb498ce93e3bdd
cerwad <cedric.barale@gmail.com>
2023-05-17 10:35:44 +0200

    First endpoint, Normal MVC working fine

--- src/main/java/fr/ced/fizzbuzz/generator/FizzBuzzValidator.java
+++ src/main/java/fr/ced/fizzbuzz/generator/FizzBuzzValidator.java
@@ -1,18 +1,31 @@
 package fr.ced.fizzbuzz.generator;
 
 import fr.ced.fizzbuzz.commons.errors.BadArgumentsException;
+import fr.ced.fizzbuzz.commons.errors.MissingRequiredArgument;
 import fr.ced.fizzbuzz.generator.model.IntMatcher;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FizzBuzzValidator {
 
-    public void validateLimit(int limit){
+    public void validateLimit(Integer limit){
+        if(limit == null) {
+            throw new MissingRequiredArgument("limit");
+        }
         if(limit > 100){
             throw new BadArgumentsException("Limit cannot exceed 100");
         }
         if(limit < 0){
             throw new BadArgumentsException("Limit cannot be negative");
+        }
+    }
+
+    public void assertNotNull(Integer int1, Integer int2){
+        if(int1 == null) {
+            throw new MissingRequiredArgument("int1");
+        }
+        if(int2 == null) {
+            throw new MissingRequiredArgument("int2");
         }
     }
 
@@ -26,15 +39,9 @@ public class FizzBuzzValidator {
         if(matcher.text() == null || matcher.text().isBlank()){
             throw new BadArgumentsException("The replacement text cannot be empty");
         }
+        if(matcher.text().length() > 256){
+            throw new BadArgumentsException("The replacement text max length is 256 char");
+        }
     }
 
-    public int parseInt(String strInt){
-        Integer number;
-        try {
-            number = Integer.parseInt(strInt);
-        } catch (NumberFormatException e){
-            throw new BadArgumentsException("Parameter "+strInt+" must be an integer");
-        }
-        return number;
-    }
 }
